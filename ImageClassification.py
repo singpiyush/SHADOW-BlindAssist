799d1c44b46e5d1c28b1529e43b73d63cc78360c
Piyush singh <piysingh@adobe.com>
2019-11-16 23:24:36 +0530

    Added API

--- ImageClassification.py
+++ ImageClassification.py
@@ -9,10 +9,11 @@ import os
 from PIL import Image
 import sys
 import time
-
+from io import BytesIO
 
 class ImageClassification:
     def __init__(self):
+        self.threshold = 0.7
         # Add your Computer Vision subscription key to your environment variables.
         if 'COMPUTER_VISION_SUBSCRIPTION_KEY' in os.environ:
             self.subscription_key = os.environ['COMPUTER_VISION_SUBSCRIPTION_KEY']
@@ -27,13 +28,23 @@ class ImageClassification:
             sys.exit()
         self.computervision_client = ComputerVisionClient(self.endpoint, CognitiveServicesCredentials(self.subscription_key))
 
-    def get_classification_result(self,image_url):
-        return self.computervision_client.describe_image(image_url )
+    def get_classification_result(self,image):
+        return self.computervision_client.detect_objects_in_stream(image )
     
-    def get_classified_name_of_image(self,image_url):
-        result = self.get_classification_result(image_url)
-        print (result)
+    def get_classified_name_of_image(self,image):
+        name = []
+        result = self.get_classification_result(image)
+        if result != None:
+            for obj in result.objects:
+                print (obj)
+                if obj.confidence > self.threshold:
+                    name.append(obj.object_property)
+        return name
 
+    def get_classified_color_of_image(self,image):
+        pass
 if __name__=="__main__":
     a = ImageClassification()
-    a.get_classified_name_of_image("https://raw.githubusercontent.com/Azure-Samples/cognitive-services-sample-data-files/master/ComputerVision/Images/landmark.jpg")
+    image_path = "./temp/test.jpeg"
+    image_data = open(image_path, "rb")
+    print(a.get_classified_name_of_image(image_data))
--- main.py
+++ main.py
@@ -1,2 +1,22 @@
+import time
+from flask import Flask, request
+from PIL import Image
+import json
+from ImageClassification import ImageClassification
+app = Flask(__name__)
 
+classification  = ImageClassification()
 
+@app.route('/predict' , methods=["POST"])
+def predict():
+    print (request.files)
+    if request.method == 'POST' and request.files['image']:
+        img = request.files['image']
+        img.save('./temp/test.png')
+        image_data = open('./temp/test.png', "rb")
+        result  = classification.get_classified_name_of_image(image_data)
+        return json.dumps(result)
+    return "Image not found"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
